aed0e0aacdfb9cb2168fed71b83ef5ae15e87f42
edo98811 <edoardofilippi77@gmail.com>
2024-10-04 14:41:28 +0200

    updated personal config initialization

--- celescope_helper/load_settings.py
+++ celescope_helper/load_settings.py
@@ -9,8 +9,11 @@ LOCATION_FILE = Path(__file__).parent / 'location.json'
 
 def load_config():
     
-    with open(LOCATION_FILE, 'r') as file:
-        config_path = json.load(file)['location']
+    try:
+        with open(LOCATION_FILE, 'r') as file:
+            config_path = json.load(file)['location']
+    except FileNotFoundError:
+        raise FileNotFoundError(f"You need to create a config file first, run init-config")
         
     print(f'laoding config from: {config_path}')
     
